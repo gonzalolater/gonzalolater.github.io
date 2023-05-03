44a8ccd7d0f04ce0e9debfd2c136c5b7eb0fc142
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2023-05-03 15:39:27 -0300

    Challenge

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -34,7 +34,47 @@ const sampleData = [
         "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
     },
 ];
+
 const [data, updateData] = useState(sampleData);
+
+const [dataFetched, updateFetched] = useState(false);
+
+async function getAllNFTs() {
+    const ethers = require("ethers");
+    //After adding your Hardhat network to your metamask, this code will get providers and signers
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    //Pull the deployed contract instance
+    let contract = new ethers.Contract(MarketplaceJSON.address, MarketplaceJSON.abi, signer)
+    //create an NFT Token
+    let transaction = await contract.getAllNFTs()
+
+    //Fetch all the details of every NFT from the contract and display
+    const items = await Promise.all(transaction.map(async i => {
+        var tokenURI = await contract.tokenURI(i.tokenId);
+        tokenURI = GetIpfsUrlFromPinata(tokenURI);
+        let meta = await axios.get(tokenURI);
+        meta = meta.data;
+
+        let price = ethers.utils.formatUnits(i.price.toString(), 'ether');
+        let item = {
+            price,
+            tokenId: i.tokenId.toNumber(),
+            seller: i.seller,
+            owner: i.owner,
+            image: meta.image,
+            name: meta.name,
+            description: meta.description,
+        }
+        return item;
+    }))
+
+    updateFetched(true);
+    updateData(items);
+}
+
+if(!dataFetched)
+    getAllNFTs();
 
 return (
     <div>
--- src/components/NFTpage.js
+++ src/components/NFTpage.js
@@ -11,6 +11,69 @@ const [data, updateData] = useState({});
 const [message, updateMessage] = useState("");
 const [currAddress, updateCurrAddress] = useState("0x");
 
+async function getNFTData(tokenId) {
+    const ethers = require("ethers");
+    //After adding your Hardhat network to your metamask, this code will get providers and signers
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const addr = await signer.getAddress();
+    //Pull the deployed contract instance
+    let contract = new ethers.Contract(MarketplaceJSON.address, MarketplaceJSON.abi, signer)
+    //create an NFT Token
+    var tokenURI = await contract.tokenURI(tokenId);
+    const listedToken = await contract.getListedTokenForId(tokenId);
+    tokenURI = GetIpfsUrlFromPinata(tokenURI);
+    let meta = await axios.get(tokenURI);
+    meta = meta.data;
+    console.log(listedToken);
+
+    let item = {
+        price: meta.price,
+        tokenId: tokenId,
+        seller: listedToken.seller,
+        owner: listedToken.owner,
+        image: meta.image,
+        name: meta.name,
+        description: meta.description,
+    }
+    console.log(item);
+    updateData(item);
+    updateDataFetched(true);
+    console.log("address", addr)
+    updateCurrAddress(addr);
+}
+
+async function buyNFT(tokenId) {
+    try {
+        const ethers = require("ethers");
+        //After adding your Hardhat network to your metamask, this code will get providers and signers
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+
+        //Pull the deployed contract instance
+        let contract = new ethers.Contract(MarketplaceJSON.address, MarketplaceJSON.abi, signer);
+        const salePrice = ethers.utils.parseUnits(data.price, 'ether')
+        updateMessage("Buying the NFT... Please Wait (Upto 5 mins)")
+        //run the executeSale function
+        let transaction = await contract.executeSale(tokenId, {value:salePrice});
+        await transaction.wait();
+
+        alert('You successfully bought the NFT!');
+        updateMessage("");
+    }
+    catch(e) {
+        alert("Upload Error"+e)
+    }
+}
+
+    const params = useParams();
+    const tokenId = params.tokenId;
+    if(!dataFetched)
+        getNFTData(tokenId);
+    if(typeof data.image == "string")
+        data.image = GetIpfsUrlFromPinata(data.image);
+
+
     return(
         <div style={{"min-height":"100vh"}}>
             <Navbar></Navbar>
